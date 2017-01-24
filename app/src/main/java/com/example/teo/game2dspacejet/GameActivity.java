d45f298c28608c95af09910cb88a8a329931afc7
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-24 19:33:07 +0700

    GameView thread

--- app/src/main/java/com/example/teo/game2dspacejet/GameActivity.java
+++ app/src/main/java/com/example/teo/game2dspacejet/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.teo.game2dspacejet;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,4 +9,33 @@ import android.support.v7.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
+    //declaring gameView
+    //tuyên bố (khai báo) gameView
+    private GameView gameView;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //Initializing game view object
+        gameView = new GameView(this);
+
+        //adding it to contentView;
+        setContentView(gameView);
+    }
+
+    //pausing the game when activity is paused.
+    //Tạm dừng trò chơi khi activity dừng.
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    //running the game when activity is resumed
+    //Chạy game khi activity tiếp tục
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
 }
