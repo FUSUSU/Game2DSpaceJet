b5ed14a9c0710f8a9b0de9460c44510dfecc509a
c Thun Nguyn <nguyenducthuan.cit@gmail.com>
2017-01-24 17:58:32 +0700

    GameView thread

--- app/src/main/java/com/example/teo/game2dspacejet/GameView.java
+++ app/src/main/java/com/example/teo/game2dspacejet/GameView.java
@@ -1,8 +1,75 @@
 package com.example.teo.game2dspacejet;
 
+import android.content.Context;
+import android.view.Surface;
+import android.view.SurfaceView;
+
 /**
  * Created by teo on 1/24/2017.
  */
 
-public class GameView {
+public class GameView extends SurfaceView implements Runnable{
+
+    //boolean variable to track if the game is playing or not
+    volatile boolean playing;
+
+    //the game thread
+    private Thread gameThread = null;
+
+    //Class constructor
+    public GameView(Context context){
+        super(context);
+    }
+
+    @Override
+    public void run() {
+        while (playing){
+            //to update the frame
+            update();
+
+            //to draw the frame
+            draw();
+
+            //to control
+            control();
+        }
+    }
+
+
+    private void update() {
+
+    }
+
+    private void draw() {
+    }
+
+    private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pause(){
+        //when the game is paused
+        //Khi game tạm dừng
+        //setting the variable to false
+        //Thiết lập giá trị sai
+        playing = false;
+        try {
+            //stopping the thread
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void resume(){
+        //When the game is resumed
+        //Starting the thread again
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
 }
